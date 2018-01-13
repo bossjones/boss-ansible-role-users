0c29b1aae6ed4d42b6b7e8888bf9016b92ffd9ac
Malcolm Jones <malcolm@adobe.com>
2018-01-13 00:49:57 -0500

    Fix: tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -12,3 +12,15 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+def test_vagrant_user(host):
+    h = host.user('vagrant')
+
+    assert h.uid == 1000
+    assert h.gid == 1000
+    assert h.group == 'vagrant'
+    assert 'sudo' in h.groups
+    assert 'adm' in h.groups
+    assert 'vagrant' in h.groups
+    assert h.home == '/home/vagrant'
+    assert h.shell == '/bin/bash'
